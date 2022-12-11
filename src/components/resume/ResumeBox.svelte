50580832a26928cce6a7911bfe99048784654675
Julien <cefadrom1@gmail.com>
2022-12-11 15:15:22 +0100

    feat: resume header

---
+++ src/components/resume/ResumeBox.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let direction: 'row' | 'column' = 'column';
+</script>
+
+
+<style>
+    div {
+        background: var(--gray);
+        box-shadow: var(--block-shadow);
+        padding: 1rem;
+        border-radius: 1rem;
+        display: flex;
+        gap: 1rem;
+    }
+</style>
+
+
+<div style:flex-direction={direction}>
+    <slot/>
+</div>
---
+++ src/components/resume/ResumeHeader.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import ResumeBox from './ResumeBox.svelte';
+</script>
+
+
+<style>
+    img {
+        height: 8rem;
+        width: 8rem;
+        border-radius: 100%;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+</style>
+
+
+<ResumeBox direction="row">
+    <img src="/resume/profile.png" alt="Julien Wolff" height="160" width="160" />
+    <div>
+        <h1>Julien WOLFF</h1>
+        <span>
+            Étudiant en 2ème année de classe préparatoire à CESI école d’ingénieur, 
+            spécialité informatique
+        </span>
+        <span>À la recherche d’un stage en IT du 3 avril au 28 juillet 2023</span>
+    </div>
+</ResumeBox>

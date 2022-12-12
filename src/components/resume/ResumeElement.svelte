b2dce3425e2f28562852e35cd509db3cc6d700a9
Julien W <cefadrom1@gmail.com>
2022-12-12 15:31:37 +0100

    feat: formation section

---
+++ src/components/resume/ResumeElement.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import ResumeBox from './ResumeBox.svelte';
+
+    export let title: string;
+</script>
+
+
+<ResumeBox>
+    <h2>{title}</h2>
+    <slot />
+</ResumeBox>
---
+++ src/components/resume/ResumeElementSection.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import type { SvelteComponentTyped } from 'svelte';
+
+    export let title: string;
+    export let subtitle: string;
+    export let icon: ConstructorOfATypedSvelteComponent;
+</script>
+
+
+<style>
+    .header {
+        display: flex;
+        align-items: center;
+        height: 2.5rem;
+        gap: .8rem;
+        margin-bottom: .5rem;
+    }
+
+    .header-text {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+    }
+
+    .icon {
+        height: 2rem;
+        width: 2rem;
+        display: grid;
+        place-items: center;
+    }
+</style>
+
+
+<div class="section">
+    <div class="header">
+        <div class="icon">
+            <svelte:component this={icon} color="var(--blue)" size="2rem" />
+        </div>
+        <div class="header-text">
+            <h3>{title}</h3>
+            <span class="small">{subtitle}</span>
+        </div>
+    </div>
+    <slot />
+</div>

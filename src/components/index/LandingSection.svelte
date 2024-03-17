106d70555c6e9e125e430bc265734394e54fa7a5
Julien W <cefadrom1@gmail.com>
2024-03-17 14:13:32 +0100

    feat: sub-presentation text

--- src/components/index/LandingSection.svelte
+++ src/components/index/LandingSection.svelte
@@ -11,6 +11,12 @@
             <br>
             Moi c'est <span class="text-gradient">Julien</span>
         </h1>
+        <div class="subtitle-container">
+            <span>👨‍🎓 Étudiant en école d'ingénieur</span>
+            <span>⌨️ Développeur fullstack</span>
+            <span>💻 Passionné d'informatique</span>
+            <span>🐈 J'adore les chats</span>
+        </div>
     </div>
 </section>
 
@@ -30,6 +36,8 @@
 
     h1 {
         font-size: 3rem;
+        font-weight: 700;
+        margin-bottom: .75rem;
     }
 
     .text-gradient {
@@ -37,5 +45,44 @@
         background-clip: text;
         background-size: 200% 100%;
         -webkit-text-fill-color: transparent;
+    }
+
+    .subtitle-container {
+        position: relative;
+    }
+
+    .subtitle-container span {
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0;
+        animation: appear 16s linear infinite;
+    }
+
+    .subtitle-container span:nth-child(2) {
+        animation-delay: 4s;
+    }
+
+    .subtitle-container span:nth-child(3) {
+        animation-delay: 8s;
+    }
+
+    .subtitle-container span:nth-child(4) {
+        animation-delay: 12s;
+    }
+
+    @keyframes appear {
+        5% {
+            opacity: 0;
+        }
+        10% {
+            opacity: 1;
+        }
+        25% {
+            opacity: 1;
+        }
+        30% {
+            opacity: 0;
+        }
     }
 </style>

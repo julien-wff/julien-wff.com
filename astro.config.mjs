e9c8508b6924ab2efe0a4e3e1adf14a797d30e60
Julien W <cefadrom1@gmail.com>
2024-03-24 19:34:12 +0100

    feat: use _redirects file and disable redirect to default locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     locales: ['en', 'fr'],
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
+      redirectToDefaultLocale: false,
     }
   }
 });
